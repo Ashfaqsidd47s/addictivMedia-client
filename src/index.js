13b0114c6aaaad80ff0d09a7bb0712a9498d9c6c
MOHAMMAD ASHFAQ <ashfaqsidd47@gmail.com>
2022-11-08 21:47:45 +0530

    changed

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Input from "./pages/Input";
+import Listing from "./pages/Listing";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Input />} />
+          <Route path="/input" element={<Input />} />
+          <Route path="/listing" element={<Listing />} />
+        </Routes>
+      </BrowserRouter>
   </React.StrictMode>
 );
 
